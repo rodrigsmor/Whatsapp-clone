e4de43f54f5616a2ab86e80e2b2e5759e9850b20
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-14 12:40:47 -0300

    Aula 03 | Implementa prototypes

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,6 +1,7 @@
 class WhatsAppController {
     constructor() {
         console.log('WhatsappCOntroller OK');
+        this.elementsPrototype();
         this.loadElements();
     }
 
@@ -10,7 +11,55 @@ class WhatsAppController {
         document.querySelectorAll('[id]').forEach(element => {
             this.el[Format.getCamelCase(element.id)] = element;
         });
+    }
 
-        console.log(this.el);
+    elementsPrototype() {
+        Element.prototype.hide = function() {
+            this.style.display = 'none';
+            return this;
+        };
+
+        Element.prototype.show = function() {
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function() {
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+            return this;
+        }
+
+        Element.prototype.on = function(events, fn) {
+            events.split(' ').forEach(event => {
+                this.addEventListener(event, fn);
+            });
+            return this;
+        }
+
+        Elements.prototype.css = function(styles) {
+            for(let name in styles) {
+                this.style[name] = styles[name];
+            }
+            return this;
+        }
+
+        Elements.prototype.addClass = function(name) {
+            this.classList.add(name);
+            return this;
+        }
+
+        Elements.prototype.removeClass = function(name) {
+            this.classList.remove(name);
+            return this;
+        }
+
+        Elements.prototype.toggleClass = function(name) {
+            this.classList.toggle(name);
+            return this;
+        }
+
+        Elements.prototype.hasClass = function(name) {
+            return this.classList.includes(name);
+        }
     }
 }
