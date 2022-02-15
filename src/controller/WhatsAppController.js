4517eff04068734da79bea89c9aea23153fe7741
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-15 09:54:10 -0300

    Aula 05 | Usa FormData e obtém dados dos painéis

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -62,6 +62,20 @@ class WhatsAppController {
         Element.prototype.hasClass = function(name) {
             return this.classList.includes(name);
         }
+
+        HTMLFormElement.prototype.getForm = function() {
+            return new FormData(this);
+        }
+
+        HTMLFormElement.prototype.toJSON = function () {
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+                json[key] = value;
+            })
+
+            return json;
+        }
     }
 
     initEvents() {
@@ -87,6 +101,28 @@ class WhatsAppController {
 
         this.el.btnClosePanelAddContact.on('click', e => {
             this.el.panelAddContact.removeClass('open');
+        });
+
+        this.el.photoContainerEditProfile.on('click', e => {
+            this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+            if(e.key === 'Enter') {
+                e.preventDefault();
+                
+                this.el.btnSavePanelEditProfile.click();
+            }
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+        });
+
+        this.el.formPanelAddContact.on('submit', e => {
+            e.preventDefault();
+            
+            let formData = new FormData(this.el.formPanelAddContact);
         })
     }
 
