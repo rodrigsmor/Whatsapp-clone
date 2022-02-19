327d9b4a0db8822f9456ebe41971072346c477e4
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-02-19 14:20:06 -0300

    Aula 31 | Lista os contatos adicionados

--- src/model/User.js
+++ src/model/User.js
@@ -36,15 +36,36 @@ export class User extends Model {
         return Firebase.db().collection('/users');
     }
 
+    static getContactsRef(id) {
+        return User.getRef()
+            .doc(id)
+            .collection('contacts')
+    }
+
     static findByEmail(email) {
         return User.getRef().doc(email);
     }
 
     addContact(contact) {
-        return User.getRef()
-            .doc(this.email)
-            .collection('contacts')
+        return User.getContactsRef(this.email)
             .doc(btoa(contact.email))
             .set(contact.toJSON());
+    }
+
+    getContacts() {
+        return new Promise((success, failure) => {
+            User.getContactsRef(this.email).onSnapshot(docs => {
+                let contacts = [];
+
+                docs.forEach(doc => {
+                    let data = doc.data();
+                    data.id = doc.id;
+                    contacts.push(data);
+                });
+
+                this.trigger('contactschange', docs);
+                success(contacts);
+            });
+        });
     }
 }
